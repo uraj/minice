2b351cdca8d73ec34d4f8b5fec5daf26aa52e7b4
uraj.wp <uraj.wp@cb5d516b-5f59-63ff-065c-cdaa7548774e>
2010-11-29 15:35:38 +0000

    CMSR configuration implemented.

--- emu/pipeline/execute.c
+++ emu/pipeline/execute.c
@@ -55,6 +55,7 @@ static uint32_t ALUcal(uint8_t aluopcode, uint32_t operand1, uint32_t operand2, 
 static uint32_t ALUcal_setCMSR(uint8_t aluopcode, uint32_t operand1, uint32_t operand2, PSW * cmsr)
 {
     uint32_t ret;
+    uint32_t C31, C32;
     switch(aluopcode)
     {
         case 0x0U:              /* and */
@@ -68,33 +69,54 @@ static uint32_t ALUcal_setCMSR(uint8_t aluopcode, uint32_t operand1, uint32_t op
         case 0x2U:              /* sub */
         case 0xaU:              /* csub */
             ret = operand1 - operand2;
-            cmsr->C = operand1 >= opreand2;
+            C31 = ((operand1 & 0x7fffffffU) - (operand2 & 0x7fffffffU)) >> 31;
+            C32 = (((uint64_t)operand1 - operand2) >> 32) & 1;
+            cmsr->C = !C32;
+            cmsr->V = C32 ^ C31;
             break;
         case 0x3U:              /* rsb */
             ret = operand2 - operand1;
-            cmsr->C = operand2 >= operand1;
+            C31 = ((operand2 & 0x7fffffffU) - (operand1 & 0x7fffffffU)) >> 31;
+            C32 = (((uint64_t)operand2 - operand1) >> 32) & 1;
+            cmsr->C = !C32;
+            cmsr->V = C32 ^ C31;
             break;
         case 0x4U:              /* add */
         case 0xbU:              /* cadd */
             ret = operand1 + operand2;
+            C31 = ((operand1 & 0x7fffffffU) + (operand2 & 0x7fffffffU)) >> 31;
+            C32 = ((uint64_t)operand1 + operand2) >> 32;
+            cmsr->C = C32;
+            cmsr->V = C32 ^ C31;
             break;
         case 0x5U:              /* adc */
             ret = operand1 + operand2 + cmsr->C;
+            C31 = ((operand1 & 0x7fffffffU) + (operand2 & 0x7fffffffU) + cmsr->C) >> 31;
+            C32 = ((uint64_t)operand1 + operand2 + cmsr->C) >> 32;
+            cmsr->C = C32;
+            cmsr->V = C32 ^ C31;
             break;
         case 0x6U:              /* sbc */
             ret = operand1 - operand2 + cmsr->C - 1;
+            C31 = ((operand1 & 0x7fffffffU) - (operand2 & 0x7fffffffU) + cmsr->C - 1) >> 31;
+            C32 = (((uint64_t)operand1 - operand2) >> 32) & 1;
+            cmsr->C = !C32;
+            cmsr->V = C32 ^ C31;
             break;
         case 0x7U:              /* rsc */
             ret = operand2 - operand1 + cmsr->C - 1;
+            C31 = ((operand2 & 0x7fffffffU) - (operand1 & 0x7fffffffU) + cmsr->C - 1) >> 31;
+            C32 = (((uint64_t)operand2 - operand1) >> 32) & 1;
+            cmsr->C = !C32;
+            cmsr->V = C32 ^ C31;
             break;
         case 0xcU:              /* or */
             ret = operand1 | operand2;
-            cmsr->Z = !ret;
             break;
         case 0xdU:              /* mov */
             ret = operand2;
             return ret;
-        case 0xeU:
+        case 0xeU:              /* clr */
             ret = operand1 & ~operand2;
             break;
         case 0xfU:              /* mvn */
@@ -103,6 +125,7 @@ static uint32_t ALUcal_setCMSR(uint8_t aluopcode, uint32_t operand1, uint32_t op
         default:
             exit(2);
     }
+    cmsr->N = ret >> 31;
     cmsr->Z = !ret;
     return ret;
 }
