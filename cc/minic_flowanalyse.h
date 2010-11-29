d9b0ddca83dc55a87fe0a9710600571f5839a822
zhangfdxk@163.com <zhangfdxk@163.com@cb5d516b-5f59-63ff-065c-cdaa7548774e>
2010-11-29 15:37:02 +0000

    编译正确

--- cc/minic_flowanalyse.h
+++ cc/minic_flowanalyse.h
@@ -3,6 +3,7 @@
 
 #include "minic_basicblock.h"
 #include "minic_regalloc.h"
+#include "minic_symtable.h"
 
 #define DEFINE 0
 #define USE 1
@@ -26,6 +27,7 @@ extern expr_list *expr_gen;
 extern expr_list *expr_kill;
 extern expr_list *expr_in;
 extern expr_list *expr_out;*/
+struct symbol_table *curr_table;
 
 extern struct var_list *var_in;
 extern struct var_list *var_out;
@@ -34,7 +36,7 @@ extern struct var_list *use;
 extern int *def_size;
 extern int *use_size;
 
-extern void var_list_sort(struct var_list *list_array , int *size);//将一个定值点链表或者变量编号链表排序
+extern void var_list_sort(struct var_list *list_array , int size);//将一个定值点链表或者变量编号链表排序
 extern void var_list_free_bynode(struct var_list_node *head);//给定一个节点，释放node以后的链
 extern void var_list_free(struct var_list *list);//释放链表
 extern void var_list_clear(struct var_list *list);//将链表清空（tail = head , head = NULL）
@@ -46,17 +48,17 @@ extern void var_list_copy(struct var_list *source , struct var_list *dest);//des
 extern void var_list_printbynode(struct var_list_node *head);//打印head以后的节点×××××有可能有用
 extern void var_list_print(struct var_list *list);//打印链表
 
-extern void var_list_merge(struct var_list *adder , struct var_list *dest)//将变量链adder和dest合并，并写入dest内
+extern void var_list_merge(struct var_list *adder , struct var_list *dest);//将变量链adder和dest合并，并写入dest内
 extern void var_list_sub(struct var_list *sub , struct var_list *subed , struct var_list *dest);//将变量链dest = sub-subed
 extern void var_list_inter(struct var_list *inter , struct var_list *dest);//将inter和dest求交，结果放入dest中
 
 
-extern void malloc_active_var();//活跃变量分析中的内存分配函数
-extern void analyse_arg(struct triarg *arg , int type , int block_index);//活跃变量分析中，对一个标号分析
-extern void analyse_arg(int expr_index , int type , int block_index);//活跃变量分析中，对一个arg分析
-extern void initial_active_var()//完成活跃变量分析的初始化部分
-extern void solve_equa_ud();//解活跃变量分析方程组
-
+//extern void malloc_active_var();//活跃变量分析中的内存分配函数
+//extern void analyse_arg(struct triarg *arg , int type , int block_index);//活跃变量分析中，对一个标号分析
+//extern void analyse_arg(int expr_index , int type , int block_index);//活跃变量分析中，对一个arg分析
+//extern void initial_active_var()//完成活跃变量分析的初始化部分
+//extern void solve_equa_ud();//解活跃变量分析方程组
+extern void analyse_actvar();//活跃变量分析
 
 /*extern void sort_list(point_list head , int size);//将一个定值点链表或者变量编号链表排序
 extern void add_set_local(point_list de , point_list add);//将de和add求并，结果在de中
