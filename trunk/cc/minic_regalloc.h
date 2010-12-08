9b567d6f66d0e419de5df1b1550249800ddb4bf5
uraj.wp <uraj.wp@cb5d516b-5f59-63ff-065c-cdaa7548774e>
2010-12-08 16:11:21 +0000

    register allocation completes. bugs fiexed.

--- trunk/cc/minic_regalloc.h
+++ trunk/cc/minic_regalloc.h
@@ -2,6 +2,7 @@
 #define __MINIC_REGALLOC_H__
 
 #include "minic_varmapping.h"
+#include "minic_flowanalyse.h"
 
 struct ralloc_info
 {
@@ -17,9 +18,9 @@ struct adjlist
 
 /* allocate max_reg registers to a ver_set with card of n,  */
 /* return the allocation resolution */
-extern struct ralloc_info reg_alloc(char ** igmatrix,
-                                    struct adjlist ** iglist,
-                                    const int n,
+extern struct ralloc_info reg_alloc(struct var_list * vlist,
+                                    int vlist_size,
+                                    const int var_num,
                                     const int max_reg);
 
 #endif
