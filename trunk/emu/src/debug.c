c1ecbbe9bff2d2825c6f5f288d18be92aaf8ad4f
uraj.wp <uraj.wp@cb5d516b-5f59-63ff-065c-cdaa7548774e>
2010-12-13 07:59:06 +0000

    load interlock implemented.

--- trunk/emu/src/debug.c
+++ trunk/emu/src/debug.c
@@ -8,17 +8,23 @@ void print_stage_info(const StageInfo * sinfo)
     return;
 }
 
-void pregs(const RegFile * storage)
+
+void pregs()
 {
-    printf("r0 :0x%08x\tr1 :0x%08x\tr2 :0x%08x\tr3 :0x%08x\n", storage->reg[0], storage->reg[1], storage->reg[2], storage->reg[3]);
-    printf("r4 :0x%08x\tr5 :0x%08x\tr6 :0x%08x\tr7 :0x%08x\n", storage->reg[4], storage->reg[5], storage->reg[6], storage->reg[7]);
-    printf("r8 :0x%08x\tr9 :0x%08x\tr10:0x%08x\tr11:0x%08x\n", storage->reg[8], storage->reg[9], storage->reg[10], storage->reg[11]);
-    printf("r12:0x%08x\tr13:0x%08x\tr14:0x%08x\tr15:0x%08x\n", storage->reg[12], storage->reg[13], storage->reg[14], storage->reg[15]);
-    printf("r16:0x%08x\tr17:0x%08x\tr18:0x%08x\tr19:0x%08x\n", storage->reg[16], storage->reg[17], storage->reg[18], storage->reg[19]);
-    printf("r20:0x%08x\tr21:0x%08x\tr22:0x%08x\tr24:0x%08x\n", storage->reg[20], storage->reg[21], storage->reg[22], storage->reg[23]);
-    printf("r24:0x%08x\tr25:0x%08x\tr26:0x%08x\tfp :0x%08x\n", storage->reg[24], storage->reg[25], storage->reg[26], storage->reg[27]);
-    printf("ip :0x%08x\tsp :0x%08x\tlr :0x%08x\tpc :0x%08x\n\n", storage->reg[28], storage->reg[29], storage->reg[30], storage->reg[31]);
-    printf("CMSR.N:%u\tCMSR.Z:%u\tCMSR.C:%u\tCMSR.V:%u\n", storage->CMSR.N, storage->CMSR.Z, storage->CMSR.C, storage->CMSR.V);
+    if(gp_reg == NULL)
+    {
+        printf("Register file not initialized yet.\n");
+        return;
+    }
+    printf("r0 :0x%08x\tr1 :0x%08x\tr2 :0x%08x\tr3 :0x%08x\n", gp_reg->reg[0], gp_reg->reg[1], gp_reg->reg[2], gp_reg->reg[3]);
+    printf("r4 :0x%08x\tr5 :0x%08x\tr6 :0x%08x\tr7 :0x%08x\n", gp_reg->reg[4], gp_reg->reg[5], gp_reg->reg[6], gp_reg->reg[7]);
+    printf("r8 :0x%08x\tr9 :0x%08x\tr10:0x%08x\tr11:0x%08x\n", gp_reg->reg[8], gp_reg->reg[9], gp_reg->reg[10], gp_reg->reg[11]);
+    printf("r12:0x%08x\tr13:0x%08x\tr14:0x%08x\tr15:0x%08x\n", gp_reg->reg[12], gp_reg->reg[13], gp_reg->reg[14], gp_reg->reg[15]);
+    printf("r16:0x%08x\tr17:0x%08x\tr18:0x%08x\tr19:0x%08x\n", gp_reg->reg[16], gp_reg->reg[17], gp_reg->reg[18], gp_reg->reg[19]);
+    printf("r20:0x%08x\tr21:0x%08x\tr22:0x%08x\tr24:0x%08x\n", gp_reg->reg[20], gp_reg->reg[21], gp_reg->reg[22], gp_reg->reg[23]);
+    printf("r24:0x%08x\tr25:0x%08x\tr26:0x%08x\tfp :0x%08x\n", gp_reg->reg[24], gp_reg->reg[25], gp_reg->reg[26], gp_reg->reg[27]);
+    printf("ip :0x%08x\tsp :0x%08x\tlr :0x%08x\tpc :0x%08x\n\n", gp_reg->reg[28], gp_reg->reg[29], gp_reg->reg[30], gp_reg->reg[31]);
+    printf("CMSR.N:%u\tCMSR.Z:%u\tCMSR.C:%u\tCMSR.V:%u\n", gp_reg->CMSR.N, gp_reg->CMSR.Z, gp_reg->CMSR.C, gp_reg->CMSR.V);
     return;
 }
 
