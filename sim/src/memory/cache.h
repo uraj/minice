39180453e703de82cf6f902b0e17012545b9b4f4
uraj.wp <uraj.wp@cb5d516b-5f59-63ff-065c-cdaa7548774e>
2010-12-19 15:45:10 +0000

    cache complete

--- sim/src/memory/cache.h
+++ sim/src/memory/cache.h
@@ -1,47 +1,37 @@
 #ifndef __MINISIM_CACHE_H__
 #define __MINISIM_CACHE_H__
 
-#include <memory/vmem.h>
+#include "vmem.h"
 
 /* capacity = 32KB blocksize = 64B linenum = 8 */
 /* if we want to build up multilevel caches, we can just use this */
 
-#define LINENUM 0x8		/*8*/
-#define SETNUM 0x40		/*64*/
-#define BLOCKSIZE 0x40	/*64*/
+#define LINENUM 0x8		/* 8  */
+#define SETNUM 0x40		/* 64 */
+#define BLOCKSIZE 0x40	/* 64 */
 
 enum CacheSwapStrategy { RAND, FIFO, LRU };
 enum CacheWriteStrategy { Write_back, Write_through };
 
-//Write-through + No-write-allocate
-//Write-back + Write-allocate
+extern enum CacheSwapStrategy swap_strategy;
+extern enum CacheWriteStrategy write_strategy;
 
-struct Cacheline
+/* Write-through + No-write-allocate */
+/* Write-back + Write-allocate */
+
+typedef struct
 {
-	char dirty;//1 bit for Write-back + Write_allocate
-	char valid;//1 bit
-	char counter;//3 bit, used by fifo and lru	
-	unsigned int tag;
-};
+	uint8_t dirty;//1 bit for Write-back + Write_allocate
+	uint8_t valid;//1 bit
+	uint8_t counter;//3 bit, used by fifo and lru	
+	uint32_t tag;
+} Cacheline;
 
-struct Cacheinfo
-{
-	int miss;
-	int hit;
-	int save_write_time;//statistic for Write_back strategy
-	//may need more
-};
-
-extern struct Cacheline Cache[SETNUM][LINENUM];//global or not?
-/*cacheinfo global or not?*/
+extern Cacheline Cache[SETNUM][LINENUM];
 
 extern void init_cache();
 
-extern struct Cacheinfo * get_cache_info();
-
-extern int cache_write(uint32_t addr, uint32_t data);/* miss: 1		hit: 0		write_through: -1 */
-extern int cache_read(uint32_t addr, uint32_t * dest);/* miss: 1	hit: 0	*/
+extern int cache_write(uint32_t addr);/* miss: 1		hit: 0		write_through: -1 */
+extern int cache_read(uint32_t addr);/* miss: 1	hit: 0	*/
 
 #endif
-
-
