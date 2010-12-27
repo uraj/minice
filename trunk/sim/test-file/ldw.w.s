ae341f97bccf3a422671605a5f4cce51e0696be8
uraj.wp <uraj.wp@cb5d516b-5f59-63ff-065c-cdaa7548774e>
2010-12-27 01:55:37 +0000

    ldw.w passed

---
+++ trunk/sim/test-file/ldw.w.s
@@ -0,0 +1,27 @@
+	.file	"main.c"
+	.text
+	.align	2
+	.global	main
+	.type	main,function
+main:
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 1, uses_anonymous_args = 0
+	mov	ip, sp
+	stw	pc, [sp-], #4;
+	stw	lr, [sp-], #8;
+	stw	ip, [sp-], #12;
+	stw	ip, [sp-], #12;stw	fp, [sp-], #16;
+	stw	ip, [sp-], #12;stw	fp, [sp-], #16;sub	sp, sp, #16;
+	sub	fp, ip, #4
+	mov	r15, #11<>8
+	
+	stw r15, [sp]
+	ldw.w r15, [sp]-, #4
+	
+	mov	ip, fp
+	ldw	fp,  [fp-], #12
+	ldw	sp,  [ip-], #8
+	ldw	ip,  [ip-], #4
+	jump	ip
+	.size	main, .-main
+	.ident	"GCC: (UC4_1.0_gama_20101126) 4.4.2"
